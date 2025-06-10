09a2db4be9d00b2c29466fe4f56975eab1422815
KronosPlayer27 <109822333+KronosPlayer27@users.noreply.github.com>
2025-06-10 13:46:14 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Atualiza o título da live automaticamente
+python3 update_title.py
+
+# Roda o vídeo em loop (480p, silencioso, otimizado)
 ffmpeg -loglevel quiet -re -stream_loop -1 -i video.mp4 \
   -vf scale=854:480 \
-  -c:v libx264 -preset superfast -b:v 1000k \
+  -c:v libx264 -preset ultrafast -b:v 800k \
   -c:a aac -b:a 128k -f flv \
   rtmp://live.twitch.tv/app/$KEY
